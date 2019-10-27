7eb45c165eb05b9a39fcda4e1dfe08bb5aafd651
SedemQuame <sedem.amekpewu.3@gmail.com>
2019-10-27 12:08:28 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <!-- Required Meta Tags -->
+    <meta charset="utf-8">
+    <title>Inkspace</title>
+
+    <!-- Stylesheets -->
+    <!-- Frameworks -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="lib/css/animate.css">
+
+    <!-- Custom -->
+    <link rel="stylesheet" href="lib/css/index.css">
+  </head>
+  <body class="container">
+
+    <div class="row">
+      <div class="col-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6 head wrapper">
+        <p class="header-text text-center text-uppercase">InkSpace</p>
+        <p class="timer-text text-center">Timer</p>
+      </div>
+
+      <div class="col-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6 gameboard wrapper">
+        <div class="row">
+          
+        </div>
+      </div>
+
+      <div class="col-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6 controls wrapper">
+        <p>Controls</p>
+      </div>
+    </div>
+
+    <!-- Custom Javascript -->
+    <!-- Main.js -->
+    <script src="lib/js/main.js" charset="utf-8"></script>
+
+    <!-- Optional Javascript -->
+    <!-- Jquery, PopperJs, Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
